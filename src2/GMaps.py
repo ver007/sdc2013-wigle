65e067736bd44decbc4c99ecc8d965b3790479f1
xeroxnir <xeroxnir@kiara.(none)>
2013-08-23 16:46:51 -0300

    cleaning

--- src2/GMaps.py
+++ src2/GMaps.py
@@ -23,15 +23,6 @@ class MapsWindow:
 		self.aps = positive
 
   def show(self):
-
-    # define a series of location markers and their styles
-    # syntax:  markers=markerStyles|markerLocation1|markerLocation2|... etc.
-	marker_list = []
- 		
-	marker_list.append("markers=color:blue|label:S|11211|11206|11222") # blue S at several zip code's centers
-	marker_list.append("markers=size:mid|label:red| label: HOLA |color:0xFFFF00|40.702147,-74.015794|") # tiny yellow B at lat/long
-	marker_list.append("markers=size:mid|color:red|label:6|Brooklyn+Bridge,New+York,NY") # mid-sized red 6 at search location
-    # see http://code.google.com/apis/maps/documentation/staticmaps/#Markers
     
 	g = PyMap()                         # creates an icon & map by default
 	icon2 = Icon('icon')               # create an additional icon
@@ -43,21 +34,16 @@ class MapsWindow:
 
 	for pos in self.aps:
 		print  pos['lat']
-		#print "FLOAT %s" %float(pos['lat'])
 		print pos['long']
-		#print "FLOAT %s" %float(pos['long'])
 	
 		longitud = float(pos['long'])
 		latitud = float(pos['lat'])
 		point = [latitud,longitud, pos['ssid']]    
 		g.maps[0].setpoint(point)               # add the points to the map
 
-	g.maps[0].setpoint([-34,-58,'Wifi'])
-
   ##    print g.showhtml()
 	open('map.htm','wb').write(g.showhtml())   # generate test file
 
 	webbrowser.open("map.htm")
 
-#	call(["firefox", "map.htm"])
 
